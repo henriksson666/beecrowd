640875952cc264c3f0b7f3d13f906926cd2c205d
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-10-21 15:18:10 -0300

    Create Main.java

---
+++ 1008_Salary/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int employeeNumber = scanner.nextInt();
+        int workedHours = scanner.nextInt();
+        double amountPerHour = scanner.nextDouble();
+
+        double salary = workedHours * amountPerHour;
+
+        System.out.println("NUMBER = " + employeeNumber);
+        System.out.printf("SALARY = U$ %.2f%n", salary);
+
+        scanner.close();
+    }
+}
