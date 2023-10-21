b0a90787caf271f3cb66443b29e1f8a8972d8645
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-10-21 15:11:50 -0300

    Create Main.java

---
+++ 1005_Average_1/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        
+        double weightedAverage = ((A * 3.5) + (B * 7.5)) / (3.5 + 7.5);
+        
+        System.out.printf("MEDIA = %.5f%n", weightedAverage);
+
+        scanner.close();
+    }
+}
