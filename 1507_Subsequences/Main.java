157eeb4dc54dc822d38d2ad936718b1a5f7be4c4
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-11-09 13:46:38 -0300

    Create Main.java

---
+++ 1507_Subsequences/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N, Q, offset;
+        boolean found;
+
+        N = scanner.nextInt();
+
+        for (; N > 0; N--) {
+
+            String seq = scanner.next();
+            Q = scanner.nextInt();
+
+            for (; Q > 0; Q--) {
+                String sub = scanner.next();
+                offset = 0;
+                found = false;
+
+                for (char c : seq.toCharArray())
+                    if (c == sub.charAt(offset) && ++offset == sub.length()) {
+                        found = true;
+                        break;
+                    }
+
+                if (found)
+                    System.out.println("Yes");
+                else
+                    System.out.println("No");
+            }
+        }
+
+        scanner.close();
+    }
+}
