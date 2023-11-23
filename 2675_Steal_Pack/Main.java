3778e4b2ce8b4047746860ba9dd9814bc1bde535
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-11-23 10:19:34 -0300

    Create Main.java

---
+++ 2675_Steal_Pack/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextInt()) {
+            int N = scanner.nextInt();
+            long ans = 0;
+            int[] n = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                n[i] = scanner.nextInt();
+            }
+
+            int mesa = n[N - 1];
+
+            for (int i = N - 2; i >= 0; i--) {
+                if (n[i] < mesa) {
+                    mesa = n[i];
+                } else {
+                    ans += n[i];
+                }
+            }
+
+            System.out.println(ans);
+        }
+
+        scanner.close();
+    }
+}
